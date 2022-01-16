dd6fff4fc08744c1bbb4302de654d2686507e882
GlebTanaka <gleb5655@gmail.com>
2022-01-16 21:01:47 +0100

    Add visualizer

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ const songContainer2 = new SongContainer('2');
 
 // Initially load song details into DOM
 songContainer1.loadSong(0);
+songContainer1.drawVisualizer();
 songContainer2.loadSong(0);
 
 
--- src/songContainer.js
+++ src/songContainer.js
@@ -30,34 +30,109 @@ class SongContainer {
 		//this.audioCtx.addEventListener('timeupdate', this.updateProgress.bind(this));
 		// Song ends
 		// audio.addEventListener('ended', nextSong);
+
+        this.lowerBandThreshold = 500;
+        this.higherBandThreshold = 3000;
+        /* https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas */
+        // Get a canvas defined with ID "visualizer"
+        this.canvas = document.getElementById('visualizer');
+        //"2d", leading to the creation of a CanvasRenderingContext2D object representing a two-dimensional rendering context
+        this.canvasContext = this.canvas.getContext('2d');
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        /*
+        A node able to provide real-time frequency and time-domain analysis information
+        https://developer.mozilla.org/en-US/docs/Web/API/AudioNode
+        */
+        this.analyzerNode = new AnalyserNode(this.audioCtx, {
+            fftSize: 256,
+            maxDecibels: -25,
+            minDecibels: -60,
+            smoothingTimeConstant: 0.8
+        });
+
+        // number of data values for the visualization (128)
+        this.arrayLength = this.analyzerNode.frequencyBinCount;
+        this.dataArray = new Uint8Array(this.arrayLength);
+        // let audioCoding = new AudioBuffer();
+        // let source;
+
+        this.volume = document.getElementById('volume');
+
+        /*
+         Node, that represents a change in volume.
+         https://developer.mozilla.org/en-US/docs/Web/API/GainNode
+         */
+        this.gain = new GainNode(this.audioCtx, {gain: this.volume.value});
 	}
+
 
 	loadSong(index) {
 		if (this.source) this.source.disconnect();
 		this.source = this.audioCtx.createBufferSource();
 	    let request = new XMLHttpRequest();
 	    let song = this.songs[index];
+        let audioCoding;
 	    request.open('GET', `music/${song}.mp3`, true);
 	    request.responseType = 'arraybuffer';
 	    request.onload = function() {
 	        this.audioCtx.decodeAudioData(request.response, function(buffer) {
 	            this.source.buffer = buffer;
+                console.log(buffer);
 	            this.source.connect(this.audioCtx.destination);
 	            this.source.loop = true;
 	        }.bind(this));
 	    }.bind(this);
 	    request.send();
-	    
+
 	    this.audioCtx.suspend();
 	    this.source.start(0);
 	    this.title.innerText = song;
 	    this.cover.src = `images/${song}.jpg`;
 	}
 
-	playSong() {
+    /**
+     * The function to call when it's time to update your animation for the next repaint.
+     * The callback function is passed one single argument
+     */
+    drawVisualizer() {
+
+        /*
+         tells the browser that you wish to perform an animation and requests that the browser calls a
+         specified function to update an animation before the next repaint.
+         The method takes a callback as an argument to be invoked before the repaint.
+         */
+        requestAnimationFrame(this.drawVisualizer)
+
+        // copies the current frequency data into 'dataArray' (Uint8Array) passed into it as parameter.
+        this.analyzerNode.getByteFrequencyData(this.dataArray)
+        console.log(this.dataArray);
+
+        const barWidth = (this.width / this.arrayLength) * 1.82;
+        let barHeight;
+        let x = 0;
+
+        this.canvasContext.fillStyle = 'rgb(0, 0, 0)';
+        this.canvasContext.fillRect(0, 0, this.width, this.height);
+        this.dataArray.forEach((item, index) => {
+            barHeight = this.dataArray[index];
+            this.canvasContext.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+            this.canvasContext.fillRect(x, this.height - barHeight / 2, barWidth, barHeight);
+            x += barWidth + 1;
+        });
+    }
+
+
+    playSong() {
 	    this.musicContainer.classList.add('play');
 	    this.playBtn.querySelector(this.querySelector).classList.remove(this.classPlay);
 	    this.playBtn.querySelector(this.querySelector).classList.add(this.classPause);
+
+        // source = audioContext.createBufferSource();
+        // // assign extracted AudioBuffer to source
+        // source.buffer = audioCoding;
+        // this.gain.connect(this.analyzerNode)
+
 	    this.audioCtx.resume();
 	}
 
